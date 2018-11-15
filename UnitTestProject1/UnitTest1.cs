0903745d8b11acc6ae657ad446beb101f48bd0b8
huseyin <gmka81@gmail.com>
2018-11-15 16:36:45 +0300

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            Console.WriteLine("hooy");
+
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Console.WriteLine("hey");
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
